1d085e37b33458d91fc50290659435bba03c4744
max <maximiliano.g.rubino@gmail.com>
2025-04-01 21:23:39 +0200

    Update main.jsx
    aplicando hashrouter para evitar conflicto entre rutas.

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { MusclesApp } from './MusclesApp';
 import './styles.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
-    <BrowserRouter>
+    <HashRouter>
     
       <MusclesApp />
 
-    </BrowserRouter>
+    </HashRouter>
 
   </StrictMode>
 )
